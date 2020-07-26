b6f4a1cb8ff70ca252afd2d693906736f2feb2d5
Florian Schroedl <flo.schroedl@gmail.com>
2020-07-26 18:01:13 +0200

    Use rpi4

--- config/sd-image.nix
+++ config/sd-image.nix
@@ -4,13 +4,13 @@
     # - generic-aarch64
     # - rpi3 (alias for generic-aarch64)
     # - rpi4
-    ./rpi3
+    ./rpi4
   ];
 
   # The installer starts with a "nixos" user to allow installation, so add the SSH key to
   # that user. Note that the key is, at the time of writing, put in `/etc/ssh/authorized_keys.d`
   users.extraUsers.nixos.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 ..."
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQ+NcxEWCFLdenXYUeKtMmwmdKcovMGUi/VWiBRUZr6Ul5Bj7iu+29QL8j7g8Xi4cMdMA98CNKhBBSfdPZu1bApP97TgUfpbKMZmnMP9xKCPNkdQyO4D0EreBC6P9zMdq9LfNWl5non+8PZmZ55eeoSCC2CwQqWjq/IQjeCR4JB4Wc2IHsiTVItA5q6FQ+5nRXz13Rhk/uxWRQStlG5XTePdglOqz9F0fJRMC1NN1FbXBMBi7Cfpg+Tc6RWKFnNYJcIYrMa2CnF0fCEqivtmfr7AutsA3ygYsucfOZzmsOm2CE0A6x80qAtungFH+MpHOr9NWX5QU7jLUvhOzhErir floscr@thinknix"
   ];
 
   # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low
